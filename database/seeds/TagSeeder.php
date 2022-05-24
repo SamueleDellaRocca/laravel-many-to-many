89636d7dd218bc7c1b0dfc687f03cc12c0f79212
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-24 16:47:08 +0200

    struttura database

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['cani', 'Roma', 'Cinema', 'php', 'Javascript', 'gatti', 'programmazione', 'codice' ];
+
+        foreach ($tags as $tag) {
+            Tag::create([
+                'name' => $tag,
+                'slug' => Str::slug($tag),
+            ]);
+        }
+    }
+}
